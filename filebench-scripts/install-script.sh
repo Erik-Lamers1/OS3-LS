ed70794710069d6df77dc08234359594df86d9cb
RonaldvdG <45560394+RonaldvdG@users.noreply.github.com>
2018-12-03 14:35:08 +0100

    Update install-script.sh
    Added comments to the code

--- filebench-scripts/install-script.sh
+++ filebench-scripts/install-script.sh
@@ -2,31 +2,38 @@
 
 apt update && upgrade
 
+## Retrieving the needed packages ##
 apt install -y libtool m4 autotools-dev wget make git bison flex
 
 cd /tmp
 
+## Retrieving the filebench tarball
 wget https://github.com/filebench/filebench/releases/download/1.5-alpha3/filebench-1.5-alpha3.tar.gz
 
 tar xvfz filebench-1.5-alpha3.tar.gz
 
 cd filebench-1.5-alpha3
 
+## Compiling filebench
 ./configure && make && make install
 
 mkdir /filebench/
 mkdir /filebench/workloads/
 cd /filebench/workloads/
 
+## Retrieving the pre-defined workloads
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/fileserver.f
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/varmail.f
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/webserver.f
 
 cd /filebench/
+
+## Retrieving the benchmark script
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/benchmark.sh
 
 chmod +x benchmark.sh
 
+## Asking you if you want to run the benchmark right away
 read -p "Would you like to run the benchmark? [Y/n]" -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
