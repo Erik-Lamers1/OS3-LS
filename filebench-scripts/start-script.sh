b71f47f7c02725ab461542d3fd7b1026061f41a2
RonaldvdG <rgaag@XPS15-9570.localdomain>
2018-12-03 12:11:45 +0100

    Add filebench startscript

---
+++ filebench-scripts/start-script.sh
@@ -0,0 +1,21 @@
+apt update && upgrade
+
+apt install -y libtool m4 autotools-dev wget make git bison flex
+
+cd /tmp
+
+wget https://github.com/filebench/filebench/releases/download/1.5-alpha3/filebench-1.5-alpha3.tar.gz
+
+tar xvfz filebench-1.5-alpha3.tar.gz
+
+cd filebench-1.5-alpha3
+
+./configure && make && make install
+
+mkdir /filebench/
+mkdir /filebench/workloads/
+
+cp /usr/local/share/filebench/workloads/webserver.f /filebench/workloads/
+cp /usr/local/share/filebench/workloads/fileserver.f /filebench/workloads/
+cp /usr/local/share/filebench/workloads/varmail.f /filebench/workloads/
+
