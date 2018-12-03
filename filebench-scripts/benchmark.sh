d930a8ba8b264f82065e7e6d6cf3bea4a2c16841
Erik Lamers <4866765+Erik-Lamers1@users.noreply.github.com>
2018-12-03 15:54:54 +0100

    Make run_benchmark function

--- filebench-scripts/benchmark.sh
+++ filebench-scripts/benchmark.sh
@@ -2,6 +2,7 @@
 
 resultsdir=/filebench/results
 workloadsdir=/filebench/workloads
+declare -a server_types=("fileserver" "webserver" "mailserver")
 
 echo "Workdir = $workloadsdir"
 echo "Resultsdir = $resultsdir"
@@ -10,37 +11,29 @@ cd "$workloadsdir"
 
 ## Retrieving the newest pre-defined workloads
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/fileserver.f
-wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/varmail.f
+wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/mailserver.f
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/webserver.f
 
-if [ ! -d "$resultsdir" ]; then
+if [[ ! -d "$resultsdir" ]]; then
      mkdir "$resultsdir"
 fi
 
-echo "### Beginning with the fileserver-test ###"
-filebench -f $workloadsdir/fileserver.f &> $resultsdir/results_fileserver.txt
+function run_benchmark() {
+
+echo "### Beginning with the $1-test ###"
+filebench -f $workloadsdir/$1.f &> $resultsdir/results_$1.txt
 echo "# Done #"
 sleep 30
 rm -rf /filebench/logfiles
 rm -rm /filebench/bigfileset
 sleep 30
 
-echo "### Beginning with the mailserver-test ###"
-filebench -f $workloadsdir/varmail.f &> $resultsdir/results_mailserver.txt
-echo "# Done #"
-sleep 30
-rm -rf /filebench/logfiles
-rm -rm /filebench/bigfileset
-sleep 30
+}
 
-echo "### Beginneing with the webserver-test ###"
-filebench -f $workloadsdir/webserver.f &> $resultsdir/results_webserver.txt
-echo "# Done #"
-sleep 10
-rm -rf /filebench/logfiles
-rm -rm /filebench/bigfileset
+for i in ${server_types[@]}; do
+   run_benchmark ${i}
+done
 
 echo
 echo
 echo "Test is completed. I wrote three result_files in $resultsdir."
-
