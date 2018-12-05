2618cc239c0d956af2e2a62239075b3ba54157c0
RonaldvdG <rgaag@XPS15-9570.localdomain>
2018-12-05 14:48:49 +0100

    Fixed a bug in the file.

--- filebench-scripts/benchmark.sh
+++ filebench-scripts/benchmark.sh
@@ -8,7 +8,7 @@ echo "Workdir = $workloadsdir"
 echo "Resultsdir = $resultsdir"
 
 cd "$workloadsdir"
-rm -r "$workloadsdir/*"
+rm -r "./*"
 ## Retrieving the newest pre-defined workloads
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/fileserver.f
 wget https://raw.githubusercontent.com/Erik-Lamers1/OS3-LS/master/filebench-scripts/workloads/mailserver.f
